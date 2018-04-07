9f7c6d5564ab325ec3e98bbd87e478533f3a703a
Adrian Osmond <adrian@Adrians-MacBook-Pro.local>
2018-04-07 08:51:58 +0200

    Exercise 3

---
+++ exercise-03.js
@@ -0,0 +1,60 @@
+/**
+ * EXERCISE 03 :: FUNCTIONS
+ * 
+ * If you think back to the code you wrote in exercise 2, it wasn't very
+ * reusable. Perhaps you would need to calculate the sum of two sets of
+ * numbers. Using the techniques we've learneed so far we would have
+ * to copy and paste the code and change the variable names. Maube that
+ * wouldn't be such a bad thing to do twice, but what if you needed to do
+ * it 100 times. 
+ * 
+ * Often in programming you want to be able to reuse code and for that we
+ * have functions: https://www.javascript.com/learn/javascript/functions
+ * 
+ * In this exercise, write functions for sum, difference, product and 
+ * quotient, which take 2 numbers as parameters and return the correct
+ * value for each function. If you want to test that your code is working,
+ * you can console.log a function like so:
+ * 
+ * console.log(sum(1, 2))
+ * 
+ * However, in programming it isn't good practice to leave console.log
+ * messages in your code when it's finished, so when you're happy that
+ * it works and before you run the tests, remove them!
+ * 
+ * 👇 Write code anywhere below the next line 👇
+ **/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * 👆 Write code anywhere above the previous line 👆
+ * 
+ * You can ignore stuff that happens below here. If you change it you'll probably
+ * break the ability to test your code, so don't do it!
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+module.exports = {
+  sum: typeof(sum) !== "undefined" ? sum : undefined,
+  difference: typeof(difference) !== "undefined" ? difference : undefined,
+  product: typeof(product) !== "undefined" ? product : undefined,
+  quotient: typeof(quotient) !== "undefined" ? quotient : undefined,
+};
---
+++ exercise-03.test.js
@@ -0,0 +1,70 @@
+const mockConsole = jest.fn();
+console.log = mockConsole;
+
+const exercise03 = require('./exercise-03');
+
+describe('Exercise 2', function() {
+  test('sum is defined correctly', function() {
+    expect(exercise03.sum).toBeDefined();
+  });
+
+  test('sum takes 2 numbers as parameters', function() {
+    expect(exercise03.sum.length).toBe(2);
+  });
+  
+  test('sum works', function() {
+    expect(exercise03.sum(1, 2)).toBe(3);
+    expect(exercise03.sum(1, -1)).toBe(0);
+    expect(exercise03.sum(4, 6)).toBe(10);
+    expect(exercise03.sum(100, 100)).toBe(200);
+  });
+
+  test('difference is defined correctly', function() {
+    expect(exercise03.difference).toBeDefined();
+  });
+
+  test('difference takes 2 numbers as parameters', function() {
+    expect(exercise03.difference.length).toBe(2);
+  });
+  
+  test('difference works', function() {
+    expect(exercise03.difference(10, 8)).toBe(2);
+    expect(exercise03.difference(10, 10)).toBe(0);
+    expect(exercise03.difference(10, -10)).toBe(20);
+    expect(exercise03.difference(10, 20)).toBe(-10);
+  });
+    
+  test('product is defined correctly', function() {
+    expect(exercise03.product).toBeDefined();
+  });
+
+  test('product takes 2 numbers as parameters', function() {
+    expect(exercise03.product.length).toBe(2);
+  });
+  
+  test('product works', function() {
+    expect(exercise03.product(1, 1)).toBe(1);
+    expect(exercise03.product(1, -1)).toBe(-1);
+    expect(exercise03.product(-1, -1)).toBe(1);
+    expect(exercise03.product(10, 5)).toBe(50);
+  });
+  
+  test('quotient is defined correctly', function() {
+    expect(exercise03.quotient).toBeDefined();
+  });
+
+  test('quotient takes 2 numbers as parameters', function() {
+    expect(exercise03.quotient.length).toBe(2);
+  });
+  
+  test('quotient works', function() {
+    expect(exercise03.quotient(1, 1)).toBe(1);
+    expect(exercise03.quotient(10, 2)).toBe(5);
+    expect(exercise03.quotient(3, 2)).toBe(1.5);
+    expect(exercise03.quotient(-3, 2)).toBe(-1.5);
+  });
+    
+  test('no console logs left behind!', () => {
+    expect(mockConsole.mock.calls.length).toBe(0);
+  });
+})
