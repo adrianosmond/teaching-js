5fc737d35ad6d9034e51ded85177c8368e974583
Adrian Osmond <adrian@Adrians-MacBook-Pro.local>
2018-04-07 18:36:14 +0200

    Exercise 11

--- exercise-11.js
+++ exercise-11.js
@@ -24,15 +24,7 @@
  **/
 
 function fizzBuzz(number) {
-  if (number % 15 === 0) {
-    return "Fizz Buzz"
-  } else if (number % 5 === 0) {
-    return "Buzz"
-  } else if (number % 3 === 0) {
-    return "Fizz"
-  } else {
-    return number;
-  }
+
 }
 
 /**
